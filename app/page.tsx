6e17f987e3c0ad1e6a29654e7db5d2803fba1ace
Kiczu <adrian.mamyto@gmail.com>
2026-02-06 12:57:44 +0100

    feat: wire home sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import { Container } from "@mui/material";
 import HeroStack from "./components/home/HeroStack";
 import BestSellersSection from "./components/home/BestSellersSection";
+import FreeProductsSection from "./components/home/FreeProductsSection";
+import AboutSection from "./components/home/AboutSection";
+import ContactSection from "./components/home/ContactSection";
 
 const HomePage = () => {
   return (
@@ -10,7 +13,10 @@ const HomePage = () => {
       <HeroStack />
       <Container maxWidth="xl">
         <div id="home-next"></div>
+        <FreeProductsSection />
         <BestSellersSection />
+        <AboutSection />
+        <ContactSection />
       </Container>
     </>
   );
